e5e5aa5d5bde7f4a1ed001cf260940399b9685cf
Joao Pedro <jp.ssilva@live.com>
2020-02-18 14:43:00 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Políticas do Site";
+            ViewData["Author"] = "Heart 'n Soul Productions";
 
             return View();
         }
